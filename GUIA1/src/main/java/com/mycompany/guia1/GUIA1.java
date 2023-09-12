32942e49a0e7930b53212f86b0ff2eb6b6a7baae
carlosperaltarobles <peraltacarlos30@gmail.com>
2023-09-12 08:58:52 -0600

    lab

---
+++ GUIA1/src/main/java/com/mycompany/guia1/GUIA1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia1;
+import com.mycompany.guia1.JFrameNotas;
+
+/**
+ *
+ * @author sis
+ */
+public class GUIA1 {
+
+    public static void main(String[] args) {
+        JFrameNotas f= new JFrameNotas();
+        f.setVisible(true);
+    }
+}
